e995dbde675909f33d259d90c46795cac41a0a15
taibeled <179261820+taibeled@users.noreply.github.com>
2025-02-11 17:18:06 -0500

    Fix PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,23 @@ export default defineConfig({
                 forward: ["dataLayer.push"],
             },
         }),
-        AstroPWA(),
+        AstroPWA({
+            manifest: {
+                name: "Jet Lag Hide and Seek Map Generator",
+                short_name: "Map Generator",
+                description:
+                    "Automatically generate maps for Jet Lag The Game: Hide and Seek with ease! Simply name the questions and watch the map eliminate hundreds of possibilities in seconds.",
+                display: "standalone",
+                start_url: "/",
+                icons: [
+                    {
+                        src: "/JetLagHideAndSeek/JLIcon.png",
+                        sizes: "1080x1080",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
     ],
     devToolbar: {
         enabled: false,
