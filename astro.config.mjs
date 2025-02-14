bbdf9b0984261751429c4707d66bc6aa56be0219
taibeled <179261820+taibeled@users.noreply.github.com>
2025-02-13 20:41:53 -0500

    Favicon modifications

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,18 @@ export default defineConfig({
                     "Automatically generate maps for Jet Lag The Game: Hide and Seek with ease! Simply name the questions and watch the map eliminate hundreds of possibilities in seconds.",
                 icons: [
                     {
-                        src: "/JetLagHideAndSeek/JLIcon.png",
+                        src: "https://taibeled.github.io/JetLagHideAndSeek/JLIcon.png",
                         sizes: "1080x1080",
+                        type: "image/png",
+                    },
+                    {
+                        src: "https://taibeled.github.io/JetLagHideAndSeek/android-chrome-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "https://taibeled.github.io/JetLagHideAndSeek/android-chrome-512x512.png",
+                        sizes: "512x512",
                         type: "image/png",
                     },
                 ],
