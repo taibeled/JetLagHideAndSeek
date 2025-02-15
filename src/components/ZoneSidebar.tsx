367183d9fa974744dbed8376a0f6413db7b05557
PeculiarProgrammer <179261820+taibeled@users.noreply.github.com>
2025-02-15 11:11:47 -0500

    Display no stations button

--- src/components/ZoneSidebar.tsx
+++ src/components/ZoneSidebar.tsx
@@ -68,7 +68,7 @@ export const ZoneSidebar = () => {
     const stations = useStore(trainStations);
     const setStations = trainStations.set;
 
-    const showGeoJSON = (geoJSONData: any) => {
+    const removeHidingZones = () => {
         if (!map) return;
 
         map.eachLayer((layer: any) => {
@@ -77,6 +77,12 @@ export const ZoneSidebar = () => {
                 map.removeLayer(layer);
             }
         });
+    };
+
+    const showGeoJSON = (geoJSONData: any) => {
+        if (!map) return;
+
+        removeHidingZones();
 
         const geoJsonLayer = geoJSON(geoJSONData, {
             style: {
@@ -381,7 +387,15 @@ export const ZoneSidebar = () => {
                             </SidebarMenuItem>
                             {stations.length > 0 && (
                                 <SidebarMenuItem
-                                    className="bg-popover hover:bg-accent relative flex cursor-default gap-2 select-none items-center rounded-sm px-2 py-2.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected='true']:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+                                    className="bg-popover hover:bg-accent relative flex cursor-pointer gap-2 select-none items-center rounded-sm px-2 py-2.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected='true']:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+                                    onClick={removeHidingZones}
+                                >
+                                    No Stations
+                                </SidebarMenuItem>
+                            )}
+                            {stations.length > 0 && (
+                                <SidebarMenuItem
+                                    className="bg-popover hover:bg-accent relative flex cursor-pointer gap-2 select-none items-center rounded-sm px-2 py-2.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected='true']:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
                                     onClick={() => {
                                         const bbox = turf.bbox(
                                             turf.featureCollection(stations),
@@ -447,6 +461,7 @@ export const ZoneSidebar = () => {
                                                             }
                                                         });
                                                     }}
+                                                    className="cursor-pointer"
                                                 >
                                                     {station.properties
                                                         .properties[
