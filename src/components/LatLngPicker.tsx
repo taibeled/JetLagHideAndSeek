dbfdd4b6870727dae97470ef462995f6c160aa75
taibeled <179261820+taibeled@users.noreply.github.com>
2025-02-17 15:48:43 -0500

    Direction selector

--- src/components/LatLngPicker.tsx
+++ src/components/LatLngPicker.tsx
@@ -6,6 +6,13 @@ import {
 } from "./ui/sidebar-l";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "./ui/select";
 
 export const LatitudeLongitude = ({
     latitude,
@@ -29,21 +36,75 @@ export const LatitudeLongitude = ({
                 <Label className="leading-5">{latLabel}</Label>
                 <Input
                     type="number"
-                    value={latitude}
+                    value={Math.abs(latitude)}
+                    min={0}
+                    max={90}
                     onChange={(e) => {
-                        onChange(parseFloat(e.target.value), null);
+                        if (isNaN(parseFloat(e.target.value))) return;
+
+                        onChange(
+                            parseFloat(e.target.value) *
+                                (latitude !== 0 ? Math.sign(latitude) : -1),
+                            null,
+                        );
                     }}
                 />
+                <Select
+                    onValueChange={(value) =>
+                        onChange(
+                            value === "north"
+                                ? Math.abs(latitude)
+                                : -Math.abs(latitude),
+                            null,
+                        )
+                    }
+                    value={latitude > 0 ? "north" : "south"}
+                >
+                    <SelectTrigger className="max-w-[55px]">
+                        <SelectValue placeholder="Direction"></SelectValue>
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="north">N</SelectItem>
+                        <SelectItem value="south">S</SelectItem>
+                    </SelectContent>
+                </Select>
             </SidebarMenuItem>
             <SidebarMenuItem className={MENU_ITEM_CLASSNAME}>
                 <Label className="leading-5">{lngLabel}</Label>
                 <Input
                     type="number"
-                    value={longitude}
+                    value={Math.abs(longitude)}
+                    min={0}
+                    max={180}
                     onChange={(e) => {
-                        onChange(null, parseFloat(e.target.value));
+                        if (isNaN(parseFloat(e.target.value))) return;
+
+                        onChange(
+                            null,
+                            parseFloat(e.target.value) *
+                                (longitude !== 0 ? Math.sign(longitude) : -1),
+                        );
                     }}
                 />
+                <Select
+                    onValueChange={(value) =>
+                        onChange(
+                            null,
+                            value === "east"
+                                ? Math.abs(longitude)
+                                : -Math.abs(longitude),
+                        )
+                    }
+                    value={longitude > 0 ? "east" : "west"}
+                >
+                    <SelectTrigger className="max-w-[55px]">
+                        <SelectValue placeholder="Direction"></SelectValue>
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="east">E</SelectItem>
+                        <SelectItem value="west">W</SelectItem>
+                    </SelectContent>
+                </Select>
             </SidebarMenuItem>
             <SidebarMenuItem>
                 <SidebarMenuButton
