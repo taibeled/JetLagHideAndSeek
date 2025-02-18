1646e2cd409734ea1c32b1088cbd180e96d8ca84
taibeled <179261820+taibeled@users.noreply.github.com>
2025-02-18 10:23:26 -0500

    No need for recentering in hiding zone sidebar

--- src/components/ZoneSidebar.tsx
+++ src/components/ZoneSidebar.tsx
@@ -444,18 +444,6 @@ export const ZoneSidebar = () => {
                                 <SidebarMenuItem
                                     className="bg-popover hover:bg-accent relative flex cursor-pointer gap-2 select-none items-center rounded-sm px-2 py-2.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected='true']:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
                                     onClick={() => {
-                                        const bbox = turf.bbox(
-                                            turf.featureCollection(stations),
-                                        );
-
-                                        const bounds: [
-                                            [number, number],
-                                            [number, number],
-                                        ] = [
-                                            [bbox[1], bbox[0]],
-                                            [bbox[3], bbox[2]],
-                                        ];
-
                                         setCommandValue("");
                                         showGeoJSON(
                                             turf.featureCollection(
@@ -520,12 +508,6 @@ export const ZoneSidebar = () => {
                                                 },
                                             },
                                         );
-
-                                        if (animateMapMovements.get()) {
-                                            map?.flyToBounds(bounds);
-                                        } else {
-                                            map?.fitBounds(bounds);
-                                        }
                                     }}
                                 >
                                     All Stations
@@ -535,18 +517,6 @@ export const ZoneSidebar = () => {
                                 <SidebarMenuItem
                                     className="bg-popover hover:bg-accent relative flex cursor-pointer gap-2 select-none items-center rounded-sm px-2 py-2.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected='true']:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
                                     onClick={() => {
-                                        const bbox = turf.bbox(
-                                            turf.featureCollection(stations),
-                                        );
-
-                                        const bounds: [
-                                            [number, number],
-                                            [number, number],
-                                        ] = [
-                                            [bbox[1], bbox[0]],
-                                            [bbox[3], bbox[2]],
-                                        ];
-
                                         setCommandValue("");
                                         showGeoJSON(
                                             turf.featureCollection(
@@ -560,12 +530,6 @@ export const ZoneSidebar = () => {
                                             ),
                                             true,
                                         );
-
-                                        if (animateMapMovements.get()) {
-                                            map?.flyToBounds(bounds);
-                                        } else {
-                                            map?.fitBounds(bounds);
-                                        }
                                     }}
                                 >
                                     All Zones
@@ -575,18 +539,6 @@ export const ZoneSidebar = () => {
                                 <SidebarMenuItem
                                     className="bg-popover hover:bg-accent relative flex cursor-pointer gap-2 select-none items-center rounded-sm px-2 py-2.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected='true']:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
                                     onClick={() => {
-                                        const bbox = turf.bbox(
-                                            turf.featureCollection(stations),
-                                        );
-
-                                        const bounds: [
-                                            [number, number],
-                                            [number, number],
-                                        ] = [
-                                            [bbox[1], bbox[0]],
-                                            [bbox[3], bbox[2]],
-                                        ];
-
                                         setCommandValue("");
                                         showGeoJSON(
                                             unionize(
@@ -595,12 +547,6 @@ export const ZoneSidebar = () => {
                                                 ),
                                             ),
                                         );
-
-                                        if (animateMapMovements.get()) {
-                                            map?.flyToBounds(bounds);
-                                        } else {
-                                            map?.fitBounds(bounds);
-                                        }
                                     }}
                                 >
                                     No Overlap
