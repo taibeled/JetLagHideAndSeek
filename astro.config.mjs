46f4d5a4202699cb8a657ac9e091c520a42642ff
Oisín Quinn <oisin@Oisins-Mac-mini.local>
2025-03-09 01:09:07 +0000

    Add netlify deploy bits

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,47 +6,52 @@ import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import AstroPWA from "@vite-pwa/astro";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        react(),
-        tailwind({
-            applyBaseStyles: false,
-        }),
-        partytown({
-            config: {
-                forward: ["dataLayer.push"],
-            },
-        }),
-        AstroPWA({
-            manifest: {
-                name: "Jet Lag Hide and Seek Map Generator",
-                short_name: "Map Generator",
-                description:
-                    "Automatically generate maps for Jet Lag The Game: Hide and Seek with ease! Simply name the questions and watch the map eliminate hundreds of possibilities in seconds.",
-                icons: [
-                    {
-                        src: "https://taibeled.github.io/JetLagHideAndSeek/JLIcon.png",
-                        sizes: "1080x1080",
-                        type: "image/png",
-                    },
-                    {
-                        src: "https://taibeled.github.io/JetLagHideAndSeek/android-chrome-192x192.png",
-                        sizes: "192x192",
-                        type: "image/png",
-                    },
-                    {
-                        src: "https://taibeled.github.io/JetLagHideAndSeek/android-chrome-512x512.png",
-                        sizes: "512x512",
-                        type: "image/png",
-                    },
-                ],
-            },
-        }),
-    ],
-    devToolbar: {
-        enabled: false,
-    },
-    site: "https://taibeled.github.io",
-    base: "JetLagHideAndSeek",
+  integrations: [
+      react(),
+      tailwind({
+          applyBaseStyles: false,
+      }),
+      partytown({
+          config: {
+              forward: ["dataLayer.push"],
+          },
+      }),
+      AstroPWA({
+          manifest: {
+              name: "Jet Lag Hide and Seek Map Generator",
+              short_name: "Map Generator",
+              description:
+                  "Automatically generate maps for Jet Lag The Game: Hide and Seek with ease! Simply name the questions and watch the map eliminate hundreds of possibilities in seconds.",
+              icons: [
+                  {
+                      src: "https://taibeled.github.io/JetLagHideAndSeek/JLIcon.png",
+                      sizes: "1080x1080",
+                      type: "image/png",
+                  },
+                  {
+                      src: "https://taibeled.github.io/JetLagHideAndSeek/android-chrome-192x192.png",
+                      sizes: "192x192",
+                      type: "image/png",
+                  },
+                  {
+                      src: "https://taibeled.github.io/JetLagHideAndSeek/android-chrome-512x512.png",
+                      sizes: "512x512",
+                      type: "image/png",
+                  },
+              ],
+          },
+      }),
+  ],
+
+  devToolbar: {
+      enabled: false,
+  },
+
+  site: "https://taibeled.github.io",
+  base: "JetLagHideAndSeek",
+  adapter: netlify(),
 });
