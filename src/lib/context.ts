f0f15fa53a84c2f42ab63ae62f72ff1246192822
azyrite <azyrite@astrial.org>
2025-07-17 13:30:39 +1000

    fix: persist hidingRadiusUnits

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -161,15 +161,17 @@ export const hidingZone = computed(
         additionalMapGeoLocations,
         disabledStations,
         hidingRadius,
+        hidingRadiusUnits,
         displayHidingZonesOptions,
     ],
-    (q, geo, loc, altLoc, disabledStations, radius, zoneOptions) => {
+    (q, geo, loc, altLoc, disabledStations, radius, hidingRadiusUnits, zoneOptions) => {
         if (geo !== null) {
             return {
                 ...geo,
                 questions: q,
                 disabledStations: disabledStations,
                 hidingRadius: radius,
+                hidingRadiusUnits,
                 zoneOptions: zoneOptions,
             };
         } else {
@@ -180,6 +182,7 @@ export const hidingZone = computed(
                 ...$loc,
                 disabledStations: disabledStations,
                 hidingRadius: radius,
+                hidingRadiusUnits,
                 alternateLocations: structuredClone(altLoc),
                 zoneOptions: zoneOptions,
             };
